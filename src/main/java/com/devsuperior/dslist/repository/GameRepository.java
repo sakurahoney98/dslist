a08139e2119cf59147a2ee2e57fe2d81f70f36fb
Caroline de Jesus Santana <caroline.santana@inema.ba.gov.br>
2025-01-22 14:55:51 -0300

    Aula 1

---
+++ src/main/java/com/devsuperior/dslist/repository/GameRepository.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslist.entitites.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+	
+	
+	
+	
+
+}
